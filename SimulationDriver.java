ca82af6f752ac98fe8d46330847ed1d8fece66f6
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-09 15:06:23 -0700

    Create SimulationDriver.java
    Created the "SimulationDriver" class, including the most basic description of what the class should posses/be capable of [from the A1 Google doc]
    
    With the addition of this commit, files containing basic descriptions/requirements have been added to the repo for all the necessary classes delineated in the A1 Document: Question, SimulationDriver, Student, and VotingService

---
+++ SimulationDriver.java
@@ -0,0 +1,10 @@
+*/ You need a SimulationDriver to automatically simulate the whole process. 
+  Basically, the driver will:
+        1) create a question type and configure the answers
+        2) configure the question for iVote Service
+        3) randomly generate a number students and the answers
+        4) submit all the students’ answers to iVote Service
+        5) call the Voting Service output function to display the result
+
+  SimulationDriver should contain a main() method that triggers the whole process. 
+  You can randomly generate the IDs and answers for a group of students to simulate the submission process and check the results. /*
