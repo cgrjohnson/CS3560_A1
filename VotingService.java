8304777dcf94c192d4e25b8e32d3ff37a2a038e5
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-09 15:00:10 -0700

    Create VotingService.java
    Created the "VotingService" class, including the most basic description of what the class should posses/be capable of [from the A1 Google doc]

---
+++ VotingService.java
@@ -0,0 +1,9 @@
+*/ The Voting Service can be first configured with a given question type and candidate answers. 
+Then, it can start accepting submissions from students. 
+The important thing to keep in mind about accepting submissions is that each student can only submit one answer. 
+If multiple submissions are received from the same student, only the last submission will be counted.
+
+The Voting Service can also output the statistics of the submission results. 
+You don’t have to use the graphical interface. 
+Simply use the standard output (System.out) to print out the count for each answer. 
+For example, “A : 5, B : 12”, or “1. Right : 15, 2. Wrong : 20”. Feel free to customize the format for your output. /*
