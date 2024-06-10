53453d47cce7a5220bb4136084cfebcb4d9ad7dc
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-09 17:15:47 -0700

    Update VotingService.java
    Declare VotingService class, comment class abilities structure

--- VotingService.java
+++ VotingService.java
@@ -7,3 +7,16 @@ The Voting Service can also output the statistics of the submission results.
 You don’t have to use the graphical interface. 
 Simply use the standard output (System.out) to print out the count for each answer. 
 For example, “A : 5, B : 12”, or “1. Right : 15, 2. Wrong : 20”. Feel free to customize the format for your output. /*
+
+public class VotingService{
+  //configure question types, variables: type and -actual- question
+  
+  //take submissions from students 
+
+  //check for prior submission
+    //if one exists, save new answer to student ID, discard priorSub value
+
+  //output statistics
+  //add graphical interface if you have time
+
+}
