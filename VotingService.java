4bf8b4e85a9dbbb5085782829a8df85c7deb5f41
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-09 20:05:22 -0700

    Update VotingService.java
    declaration of variables, etxr., check for priorSub; replace with new sub if applicable

--- VotingService.java
+++ VotingService.java
@@ -9,13 +9,28 @@ Simply use the standard output (System.out) to print out the count for each answ
 For example, “A : 5, B : 12”, or “1. Right : 15, 2. Wrong : 20”. Feel free to customize the format for your output. /*
 
 public class VotingService{
-  //configure question types, variables: type and -actual- question
+    private int typeOfQuestion = 1;                                          //intialize question type to MC
+    private Question questionn;                                              //declare Question
+
+    //configure question types, variables: type and -actual- question
+    void configurationService(int tipo, Question newQ) {
+        typeOfQuestion = tipo;
+        question = newQ;
+    }
+
+    //check for prior submissions from participant  
+    void submission(Student participant){
+      if(participant.getSubmitted) == false) {    //no prior submission occurred
+          question.updateAnswersToQuestion(participant.getPriorSub(), -1);
+          participant.setSubmitted(true);                                    //show that a submission by student exists
+      }
+      else(){    //prior submission exists
+          question.updateAnswersToQuestion(participant.getPriorSub(), -1);
+          question.updateAnswersToQuestion(participant.getAnswer(), 1);      //replace priorSub with new input
+
+      }
+    }
   
-  //take submissions from students 
-
-  //check for prior submission
-    //if one exists, save new answer to student ID, discard priorSub value
-
   //output statistics
   //add graphical interface if you have time
 
