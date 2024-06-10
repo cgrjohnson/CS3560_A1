ee9d3c48008ed6faf0dc5b7e44c2801e09ebf95c
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-09 17:32:07 -0700

    Update Question.java
    Declare Question class, declare and initialize variables, add comments TO ADD LATER getter and setter methods for question and the type of question, add and get optionsForQuestion, ?increment questions after response or should that go to driver?

--- Question.java
+++ Question.java
@@ -3,3 +3,20 @@
     -Support 2 types of questions: 
           1) multiple-choice question (more than one choice can be chosen including one choice), “A”, “B”, “C”, “D”
           2) single choice question (only one choice can be chosen) “1. Right”, “2. Wrong”   /*
+
+public class Question{
+    private int typeOfQuestion;                                             //MC = 1, Single Answer = 2
+    private String Question;                                                //String to hold question asked
+    private Vector<Integer> optionsForQuestion = new Vector<integer>(0);    //options given for MC questions
+    private Vector<String> answersToQuestion = new Vector<String>(0);       //possible answers for single answer Qs
+
+    public Question{
+        typeOfQuestion = 1;                                                //questions are MC by default via init.
+    }
+
+    //getter & setter method for typeOfQuestion
+    //getter & setter method for Question
+
+    //add and get optionsForQuestion
+
+}
